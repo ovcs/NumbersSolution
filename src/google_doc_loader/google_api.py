9bca3cf097fbc2601c734f6e47f365cbd259fdcd
OvcS <gerkules007@yahoo.com>
2022-06-22 02:41:49 +0300

    add class GoogleAPI init

--- src/google_doc_loader/google_api.py
+++ src/google_doc_loader/google_api.py
@@ -1,1 +1,10 @@
+class GoogleAPI:
+    service = None
+
+    def __init__(self, cfg):
+        self.CREDENTIALS_SERVICE_PATH = cfg.CREDENTIALS_SERVICE_PATH
+        self.CREDENTIALS_USER_PATH = cfg.CREDENTIALS_USER_PATH
+        self.CREDENTIALS_SECRET_FILE_PATH = cfg.CREDENTIALS_SECRET_FILE_PATH
+        self.SCOPES = cfg.SCOPES
+
 
