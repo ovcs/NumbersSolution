f63e1bd8dd758df1fb3eec89d8f37958af74d193
OvcS <gerkules007@yahoo.com>
2022-06-22 03:11:07 +0300

    add CurrencyExchange.send_request()

--- src/exchange_rate/сurrency.py
+++ src/exchange_rate/сurrency.py
@@ -1,3 +1,5 @@
+import requests
+
 from datetime import date, time
 from decimal import Decimal
 
@@ -15,3 +17,10 @@ class CurrencyExchange:
         self.exchange_date = cfg.exchange_date
         self.exchange_time = cfg.exchange_time
         self.value = Decimal('1')
+
+    def send_request(self):
+        response = requests.get(self.url, headers={'Connection': 'close'})
+        if response.status_code == 200:
+            return response
+        else:
+            raise Exception(f'Error currency update, http status code {response.status_code}')
