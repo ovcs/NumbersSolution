7ffbd04e028d3ddb403a9ca0f00dc87adde7d79b
OvcS <gerkules007@yahoo.com>
2022-06-22 03:29:27 +0300

    add class DataBase init

--- src/database_ctrl/database.py
+++ src/database_ctrl/database.py
@@ -1,1 +1,9 @@
 class DataBase:
+    host: tuple
+    queries: dict
+    keys: tuple
+
+    def __init__(self, cfg):
+        self.host = cfg.queries
+        self.queries = cfg.queries
+        self.keys = cfg.keys
