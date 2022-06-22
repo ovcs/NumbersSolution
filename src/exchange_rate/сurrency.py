5ee83ba66410c383f5d2065b72a620ace3682025
OvcS <gerkules007@yahoo.com>
2022-06-22 03:09:45 +0300

    add class CurrencyExchange init

--- src/exchange_rate/сurrency.py
+++ src/exchange_rate/сurrency.py
@@ -1,1 +1,17 @@
+from datetime import date, time
+from decimal import Decimal
+
+
 class CurrencyExchange:
+    exchange_date: date
+    exchange_time: time
+    currency_id: str
+    url: str
+    value: Decimal
+
+    def __init__(self, cfg):
+        self.currency_id = cfg.currency_id
+        self.url = cfg.url
+        self.exchange_date = cfg.exchange_date
+        self.exchange_time = cfg.exchange_time
+        self.value = Decimal('1')
