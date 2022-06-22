259b5b13c4413905458fbc3db51d0c34d90e1f78
OvcS <gerkules007@yahoo.com>
2022-06-22 04:54:15 +0300

    add Project.check_connect()

--- src/project.py
+++ src/project.py
@@ -20,13 +20,23 @@ class Project:
         self.db = DataBase(cfg.DataBase)
         # self.tb = TeleBot(cfg.TeleBot)
 
-        logger = logging.getLogger()
-        logger.setLevel(logging.INFO)
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.INFO)
         formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
         stdout_handler = logging.StreamHandler(sys.stdout)
         stdout_handler.setLevel(logging.DEBUG)
         stdout_handler.setFormatter(formatter)
 
-        logger.addHandler(stdout_handler)
+        self.logger.addHandler(stdout_handler)
 
+    def check_connect(self):
+        try:
+            self.gAPI.authorize_from('service_account')
+            self.currency.have_update(self.curr_datetime)
+            self.db.connect()
+            # self.tb.connect()
+            self.logger.info("Connection Success")
+        except Exception:
+            self.logger.info(f"Connection Fault. {Exception.args}")
+            exit()
